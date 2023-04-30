9d5e4befef8cfcf913a564d1a62dd64cd721d076
Scyfren <scyfren.ds@gmail.com>
2023-05-01 07:20:29 +0800

    move types into namespace for intellisense - bumped ver

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,24 @@
-import None from "./None";
+import _None from "./None";
+import * as _table from "./table";
+
 import { Draft } from "./types-external";
 import { Objectish } from "./types-internal";
 
-export { default as None } from "./None";
-export declare const nothing: typeof None;
-export * as table from "./table";
+declare namespace Immut {
+	export const None: typeof _None;
+	export const nothing: typeof None;
+	export const table: typeof _table;
 
-export declare function createDraft<T extends Objectish>(base: T): Draft<T>;
-export declare const current: typeof finishDraft;
-export declare function finishDraft<T>(draft: Draft<T>): T;
-export declare function isDraft(value: unknown): boolean;
-export declare function isDraftable(value: unknown): boolean;
-export declare function original<T>(draft: Draft<T>): T;
-export declare function produce<T>(
-	base: T,
-	recipe: (draft: Draft<T>) => typeof draft | void | undefined | (T extends undefined ? typeof None : never),
-): T;
+	export function createDraft<T extends Objectish>(base: T): Draft<T>;
+	export const current: typeof finishDraft;
+	export function finishDraft<T>(draft: Draft<T>): T;
+	export function isDraft(value: unknown): boolean;
+	export function isDraftable(value: unknown): boolean;
+	export function original<T>(draft: Draft<T>): T;
+	export function produce<T>(
+		base: T,
+		recipe: (draft: Draft<T>) => typeof draft | void | undefined | (T extends undefined ? typeof None : never),
+	): T;
+}
+
+export = Immut;
