d09099bfd6e82890dbdec8a134f0ec38799aaad4
Scyfren <scyfren.ds@gmail.com>
2023-05-03 21:43:25 +0800

    expose makeDraftSafe in table.d.ts - bump ver

--- src/table.d.ts
+++ src/table.d.ts
@@ -3,3 +3,5 @@ export declare function insert<T>(arr: Array<T>, value: T): void;
 export declare function insert<T>(arr: Array<T>, pos: number, value: T): void;
 export declare const sort: typeof table.sort;
 export declare const clear: typeof table.clear;
+
+export { makeDraftSafe } from "./makeDraftSafe";
